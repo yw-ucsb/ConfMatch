324950db8a50ff6aae441c0f7027c032b72ffb89
Yuxuan <y_yin@ucsb.edu>
2023-12-04 21:07:17 +0000

    add top5 acc eval to softmatch

--- exp_run.sh
+++ exp_run.sh
@@ -1,3 +1,4 @@
 screen python train.py --c config/usb_cv/confmatch/cifar100_400_1.yaml
 # python finetune.py --c config/usb_cv/confmatch/cifar100_400_1.yaml
 # screen python train.py --c config/usb_cv/fixmatch/fixmatch_cifar100_4000_0.yaml
+# screen python train.py --c config/usb_cv/softmatch/softmatch_cifar100_400_0.yaml
